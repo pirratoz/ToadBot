43636afe9ffff5246d9de7f4bff9ed36b4082f4c
PirraToZ <pirratoz@gmail.com>
2026-01-31 15:31:34 +0300

    feat(): set new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def parse_requirements(filename):
 
 setup(
     name="toad_bot",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=parse_requirements("requirements.txt"),
 )
