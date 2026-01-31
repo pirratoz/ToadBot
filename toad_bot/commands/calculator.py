85a346b3f5f5d6fb460b90d07450f3e66be28ba0
PirraToZ <pirratoz@gmail.com>
2026-01-31 15:30:40 +0300

    fix(): calculator settings

--- toad_bot/commands/calculator.py
+++ toad_bot/commands/calculator.py
@@ -3,6 +3,8 @@ import re
 from pyrogram.client import Client
 from pyrogram.types import Message
 
+from toad_bot.storage.config import WEB_API
+from toad_bot.api.dto import UserProfile
 
 
 class Patten:
@@ -11,6 +13,13 @@ class Patten:
 
 
 async def handle_calculate_expression(client: Client, message: Message) -> None:
+    client_me  = await client.get_me()
+    client_id = client_me.id
+
+    profile: UserProfile = await WEB_API.get_user(client_id)
+    if not profile.user.is_calculate:
+        return
+
     condition = re.findall(Patten.pattern1, message.text)[0]
 
     result_eval = eval(condition)
